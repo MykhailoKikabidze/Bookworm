a27c34325795428b2ba0e51c99d15e764b87a282
Mykhailo Kikabidze <kikabidzemisha@gmail.com>
2024-11-26 21:12:59 +0100

    Create book schema

---
+++ service/books/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+
+class BookScheme(BaseModel):
+    title: str
+    year_of_pub: int
+    url: str = ""
+    url_img: str = ""
+    num_of_pages: int
+    description: str
+    publisher: str

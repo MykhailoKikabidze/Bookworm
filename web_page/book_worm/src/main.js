3ef820c7c025a5370a626d119c0bb60d88dc74be
Marina Didyk <carrotg008@gmail.com>
2024-12-03 20:18:59 +0100

    Added function for get authors for searching

--- web_page/book_worm/src/main.js
+++ web_page/book_worm/src/main.js
@@ -3,7 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 const app = createApp(App);
-app.config.globalProperties.$link_backend = 'https://7067-94-254-233-7.ngrok-free.app';
+app.config.globalProperties.$link_backend = 'https://6ef4e603f8d687c7bbc1cca215251e6d.serveo.net';
 
 app.use(router).mount('#app')
 
